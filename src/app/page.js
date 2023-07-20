3883ec532f95b5fa6fb90173c7a6aa4cb10aa48f
Kitpakorn <kitpakorn.chopper@gmail.com>
2023-07-20 16:47:18 +0700

    Edited

--- src/app/page.js
+++ src/app/page.js
@@ -61,7 +61,7 @@ export default function Home() {
       </div>
 
       {/* //footer section */}
-      <Footer year="2023" fullName="Chayanin Suatap" studentId="12345678" />
+      <Footer year="2023" fullName="Kitpakorn Thongkot" studentId="650610749" />
     </div>
   );
 }
